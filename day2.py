3784bb4e2d58c5386ea4a75341ad2ccabb29e869
MD ABDUS SAMI QURESHI <abdussami0516@gmail.com>
2025-07-28 19:37:10 +0530

    add new file

---
+++ day2.py
@@ -0,0 +1,13 @@
+# variables
+a = 1 
+b = 2 #b is an indentifer
+
+
+
+C = "HARRY" # C is a string
+D = 3.14 # D is a float
+E = True # E is a boolean
+F = False # F is a boolean
+G = None # G is a NoneType
+H = -1 # H is a negative integer 
+
